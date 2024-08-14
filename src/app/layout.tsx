54e58d5b91f427682554b40c70c82aa886dc2e5b
Timothy <athanasiuscopy@gmail.com>
2024-08-14 16:06:11 +0100

    minor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,18 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Car Hero Animation",
   description: "Designed and developed by Techdoc",
+  metadataBase: new URL("https://weekly-issue1-animation-site.vercel.app"),
+  openGraph: {
+    images: [
+      {
+        url: "https://fecamds-rosary-challenge.onrender.com/logo.jpg",
+        width: 255,
+        height: 255,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
